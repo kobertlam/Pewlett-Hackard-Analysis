58b704e8736b2c15c8574762b3a1d50e54698e90
Kobert Lam <kobertlam@gmail.com>
2021-11-28 11:58:29 -0500

    completed section 7.2

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -16,9 +16,9 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
